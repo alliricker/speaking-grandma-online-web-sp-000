945d37b9de55eec972257c78ae4956d8d5cc0722
Allison Ricker <alliricker@gmail.com>
2020-09-21 18:43:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938"
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
 
  end
 end
