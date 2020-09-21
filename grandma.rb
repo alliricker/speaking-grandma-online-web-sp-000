7570614d6eb2d03a0e3f1708ec360749ea9d46dd
Allison Ricker <alliricker@gmail.com>
2020-09-21 18:45:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == phrase.downcase
+  return "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 
 
  end
